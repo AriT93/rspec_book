a37f0b9efcd55cf1ec0c37286936966721287b9a
AriT93 <arit93@yahoo.com>
2011-01-11 09:06:23 -0600

    fixing and refactoring and stuff

--- codebreaker/features/codebreaker_submits_guess.feature
+++ codebreaker/features/codebreaker_submits_guess.feature
@@ -8,6 +8,13 @@ Feature: code-breaker submits guess
   guess that matches the number but not the position of a number in the secret
   code the mark includes on - sign
 
+  Each position in the secret code can only be matched once.  For example, a
+  guess of 1134 agains a secret code of 1234 would get 3 plus sings: one for
+  each of the exact matches in the first, third and forth positions. The number
+  match in the second position would be ignored.
+
+
+
   Scenario Outline: submit a gues
     Given the secret code is "<code>"
     When I guess "<guess>"
@@ -41,3 +48,13 @@ Feature: code-breaker submits guess
   | 1234 | 1243  | ++-- |
   | 1234 | 1423  | +--- |
   | 1234 | 4321  | ---- |
+
+  Scenarios: matches with duplicates
+ | code | guess | mark |
+ | 1234 |  1155 | +    |
+ | 1234 |  5115 | -    |
+ | 1134 |  1155 | ++   |
+ | 1134 |  5115 | +-   |
+ | 1134 |  5511 | --   |
+ | 1134 |  1115 | ++   |
+ | 1134 |  5111 | +-   |
--- codebreaker/lib/codebreaker/marker.rb
+++ codebreaker/lib/codebreaker/marker.rb
@@ -1,11 +1,7 @@
-class Marker
-  def initialize(secret, guess)
-    @secret, @guess = secret, guess
-  end
-    def number_match_count
-      (0..3).inject(0) do |count, index|
-        count + (number_match?(index) ? 1 : 0)
-      end
+module Codebreaker
+  class Marker
+    def initialize(secret, guess)
+      @secret, @guess = secret, guess
     end
 
     def exact_match_count
@@ -13,6 +9,23 @@ class Marker
         count + (exact_match?(index) ? 1 : 0)
       end
     end
+
+    def number_match_count
+      total_match_count - exact_match_count
+    end
+
+    def total_match_count
+      count = 0
+      secret = @secret.split('')
+      @guess.split('').map do |n|
+        if secret.include?(n)
+          secret.delete_at(secret.index(n))
+          count += 1
+        end
+      end
+      count
+    end
+
 
     def exact_match?(index)
       @guess[index] == @secret[index]
@@ -22,3 +35,4 @@ class Marker
       @secret.include?(@guess[index]) && !exact_match?(index)
     end
   end
+end
